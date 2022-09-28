5738c6b99ba99b7666c261a105d22b5c23a36f74
gorantlahitesh <gorantlahitesh01@gmail.com>
2022-09-28 14:52:31 +0530

    Subtract logic added

---
+++ sub.js
@@ -0,0 +1,8 @@
+function sub(){
+var num1 = document.getElementById("firstno").value;
+var num2 = document.getElementById("secondno").value;
+
+var result = parseInt(num1) - parseInt(num2);
+document.getElementById("result").value = result;
+
+}
