742bbc502b0e53bfa64a683ad707ea3189edbb9b
gorantlahitesh <gorantlahitesh01@gmail.com>
2022-09-28 14:26:44 +0530

    Some Changes in Add.js

--- add.js
+++ add.js
@@ -1,8 +1,8 @@
 function add(){
-var num1 = getElementById("firstno").value
-var num2 = getElementById("secondno").value
+var num1 = getElementById("firstno").value;
+var num2 = getElementById("secondno").value;
 
-var result = num1 + num2
-document.getElementById("result").value = result
+var result = num1 + num2;
+document.getElementById("result").value = result;
 
 }
