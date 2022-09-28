973bed47b4c8cf19d494902a09f7a904849cab26
gorantlahitesh <gorantlahitesh01@gmail.com>
2022-09-28 14:18:05 +0530

    Addition js file

---
+++ add.js
@@ -0,0 +1,8 @@
+function add(){
+var num1 = getElementById("firstno").value
+var num2 = getElementById("secondno").value
+
+var result = num1 + num2
+document.getElementById("result").value = result
+
+}
